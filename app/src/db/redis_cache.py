78340995fb2b9b524446103d46e934485ea1e465
Timofey <whitecat751@yandex.ru>
2023-01-05 23:22:20 +0300

    Add cashe interfase

---
+++ app/src/db/redis_cache.py
@@ -0,0 +1,74 @@
+"""Create redis db DI function."""
+from functools import lru_cache
+from typing import Optional
+
+from db.abstract_cashe import AsyncCacheStorage
+from redis import asyncio
+
+redis_cache: Optional[asyncio.Redis] = None
+
+
+class RedisCacher(AsyncCacheStorage):
+    """Implements interface to redis db."""
+
+    def __init__(self, rds: asyncio.Redis):
+        """Initialaze constructor redis db interface.
+
+        Args:
+            rds (Redis): Redis db object
+        """
+        self._redis = rds
+
+    async def get(self, key, *args, **kwargs):
+        """Get recods from db.
+
+        Args:
+            key (str): key_value of records to be returned.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Any : records asoccated with key
+        """
+        return await self._redis.get(key)
+
+    async def set(
+        self,
+        key,
+        records_value,
+        expire,
+        **kwargs,
+    ):
+        """Save records to db.
+
+        Args:
+            key (_type_): key of records to be seved.
+            records_value (_type_): value to be saved.
+            expire (_type_, optional): records keeping period.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        await self._redis.set(
+            name=key,
+            value=records_value,
+            ex=expire,
+            **kwargs,
+        )
+
+
+async def get_redis() -> asyncio.Redis:
+    """Функция внедрении зависимостей.
+
+    Returns:
+        aioredis.Redis: redis db instance.
+    """
+    return redis_cache
+
+
+@lru_cache
+def get_redis_cacher() -> RedisCacher:
+    """Функция внедрении зависимостей.
+
+    Returns:
+        RedisCacher: redis db interface.
+    """
+    return RedisCacher(redis_cache)
