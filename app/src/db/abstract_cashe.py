6bc8a325339943cc98129de3c47fd4fa67fa59a2
Timofey <whitecat751@yandex.ru>
2023-01-05 17:40:04 +0300

    Update file

--- app/src/db/abstract_cashe.py
+++ app/src/db/abstract_cashe.py
@@ -1,11 +1,34 @@
+"""Difine abstract Cashe Storage."""
 from abc import ABC, abstractmethod
 
 
 class AsyncCacheStorage(ABC):
-    @abstractmethod
-    async def get(self, key: str, **kwargs):
-        pass
+    """Storage abstract class."""
 
     @abstractmethod
-    async def set(self, key: str, value: str, expire: int, **kwargs):
-        pass
+    async def get(self, key: str, **kwargs):
+        """_Abstract method to get recods from db.
+
+        Args:
+            key (str): key of records to be returned.
+            **kwargs: Arbitrary keyword arguments.
+
+        Raises:
+            NotImplementedError:  Reaise in a case the method is not implemented
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def set(self, key: str, record_value: str, expire: int, **kwargs):
+        """Abstract method to save records to db.
+
+        Args:
+            key (str): key of records to be seved.
+            record_value (str): value to be saved.
+            expire (int): records keeping period.
+            **kwargs: Arbitrary keyword arguments.
+
+        Raises:
+            NotImplementedError: Reaise in a case the method is not implemented
+        """
+        raise NotImplementedError
